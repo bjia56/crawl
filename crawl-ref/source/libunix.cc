e966461ec44960b336e73fd1f74d0b54ac02daf2
Brett Jia <dev.bjia56@gmail.com>
2025-01-03 12:30:05 -0500

    use subdir

--- crawl-ref/source/libunix.cc
+++ crawl-ref/source/libunix.cc
@@ -52,7 +52,7 @@ static struct termios game_term;
 #include <time.h>
 
 #ifdef __COSMOPOLITAN__
-#include <dce.h>
+#include <libc/dce.h>
 #endif
 
 // replace definitions from curses.h; not needed outside this file
